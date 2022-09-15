790ae1a2fd07ada0d523879e976fa230933e779e
m3m0r7 <memory.style.serv@gmail.com>
2022-09-15 13:00:11 +0900

    Update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { AppProps } from "next/app";
 import Head from "next/head";
 
 import "@/css/styles.scss"
+import "@fortawesome/fontawesome-free/css/all.css"
 
 const App: NextPage<AppProps> = (props: AppProps) => {
   const { Component, pageProps } = props
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,11 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBorderAll, faTableCells } from "@fortawesome/free-solid-svg-icons";
 import * as layouts from "@/css/layouts.module.scss";
-
 
 const Index = () => {
   return <>
     <div className={layouts.navigator}>
       <ul>
-        <li><FontAwesomeIcon icon={faBorderAll} fontSize="32px"/></li>
-        <li><FontAwesomeIcon icon={faTableCells} fontSize="32px" /></li>
+        <li><i className={`fa-solid fa-border-all ${layouts.faGradient}`}></i></li>
+        <li><i className={`fa-solid fa-table-cells ${layouts.faGradient}`}></i></li>
       </ul>
     </div>
   </>

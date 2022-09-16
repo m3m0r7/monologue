67808780b96fe67da050ad5a9f5c44f1738bd374
m3m0r7 <memory.style.serv@gmail.com>
2022-09-16 22:25:42 +0900

    Update

--- src/components/Entry/Entry.tsx
+++ src/components/Entry/Entry.tsx
@@ -31,13 +31,11 @@ const Entry: React.FC<Props> = ({ isOpened, onClose, entry }) => {
       <div className={`${entryStyle.entryPrev} ${entryStyle.inactive}`}>
         <i className="fa-solid fa-chevron-left"></i>
       </div>
+
       <div className={entryStyle.entryBody}>
         <div className={entryStyle.entryEyecatch} style={{ backgroundImage: `url(${entry.eyecatch})` }}>
           <div className={entryStyle.entryEyecatchExpand} onClick={open}>
             <i className="fa-solid fa-expand"></i>
-          </div>
-          <div className={entryStyle.entryTitleInEyecatch}>
-            {entry.title}
           </div>
           <div className={entryStyle.entryClose} onClick={onClose}>
             <i className={`fa-solid fa-close`}></i>
@@ -45,7 +43,8 @@ const Entry: React.FC<Props> = ({ isOpened, onClose, entry }) => {
         </div>
         <div className={entryStyle.divisor}></div>
         <div className={entryStyle.entryContents}>
-          <div className={entryStyle.entryDateTime}>{dayjs(entry.date).format('ddd MMMM DD, YYYY')}</div>
+          <div className={entryStyle.entryTitle}>{entry.title}</div>
+          <time className={entryStyle.entryDateTime}>{dayjs(entry.date).format('ddd MMMM DD, YYYY')}</time>
           <ul className={entryStyle.entryTags}>
             {entry.tags.map((tag, key) => <li key={key}>#{tag.name}</li>)}
           </ul>
@@ -56,6 +55,7 @@ const Entry: React.FC<Props> = ({ isOpened, onClose, entry }) => {
           </div>
         </div>
       </div>
+
       <div className={entryStyle.entryNext}><i className="fa-solid fa-chevron-right"></i></div>
       <div className={`${eyecatchStyle.eyecatchContainer} ${isExpandedEyecatch ? '' : 'hidden'}`} onClick={close}>
         <div className={eyecatchStyle.eyecatchContents} style={{ backgroundImage: `url(${entry.eyecatch})` }}>

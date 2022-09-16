10e8b78654fe8103cca645a74b54c819ea8b5f76
m3m0r7 <memory.style.serv@gmail.com>
2022-09-17 01:44:38 +0900

    Update

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -13,34 +13,40 @@ const Editor: React.FC<Props> = () => {
   }
 
   return <div className={`${entryStyle.entryContainer}`}>
-
-      <div className={entryStyle.entryBody}>
-        <div className={entryStyle.entryEyecatch}>
-          <div className={entryStyle.entryClose} onClick={() => {}}>
-            <i className={`fa-solid fa-close`}></i>
-          </div>
+    <div className={entryStyle.entryBody}>
+      <div className={entryStyle.entryEyecatch}>
+        <div className={editor.uploadableImage}>
+          <i className={"fa-solid fa-image"}></i>
         </div>
-        <div className={entryStyle.divisor}></div>
-        <div className={entryStyle.entryContents}>
-          <div className={entryStyle.entryTitle}>
-            <input type="text" placeholder="Enter a title..." className={editor.entryTitleInput} />
-          </div>
-          <time className={entryStyle.entryDateTime}>{dayjs().format('ddd MMMM DD, YYYY')}</time>
-          <ul className={entryStyle.entryTags}>
-            <li>#Choose a tag</li>
-          </ul>
+        <div className={entryStyle.entryClose} onClick={() => {}}>
+          <i className={`fa-solid fa-close`}></i>
+        </div>
+      </div>
+      <div className={entryStyle.divisor}></div>
+      <div className={entryStyle.entryContents}>
+        <div className={entryStyle.entryTitle}>
+          <input type="text" placeholder="Enter a title..." className={editor.entryTitleInput} />
+        </div>
+        <time className={entryStyle.entryDateTime}>{dayjs().format('ddd MMMM DD, YYYY')}</time>
+        <ul className={entryStyle.entryTags}>
+          <li>#Choose a tag</li>
+        </ul>
 
-          <ul>
-            <li>Plain Text</li>
-            <li>Preview</li>
-          </ul>
+        <ul className={editor.tabs}>
+          <li>
+            <span className={`${editor.tabLabel} ${editor.tabActive}`}>Plain Text</span>
+          </li>
+          <li>
+            <span className={editor.tabLabel}>Preview</span>
+          </li>
+        </ul>
 
-          <div className={entryStyle.entryText}>
-            <textarea className={editor.entryTextInput} placeholder="Enter text..." onKeyUp={autoHeight}></textarea>
-          </div>
+        <div className={entryStyle.entryText}>
+          <textarea className={editor.entryTextInput} placeholder="Enter text..." onKeyUp={autoHeight}></textarea>
         </div>
       </div>
     </div>
+  </div>
 }
 
 export default Editor;

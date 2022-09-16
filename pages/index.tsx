9892ad62b114342a56d634a3001438621328530c
m3m0r7 <memory.style.serv@gmail.com>
2022-09-16 11:32:00 +0900

    Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,24 +4,33 @@ import React from "react";
 const Index = () => {
   return <>
     <div className={layouts.navigator}>
-      <div></div>
       <ul>
         <li><a href="https://i.mem.ooo/" target="_blank"><i className={`fa-solid fa-user ${layouts.faGradient}`}></i></a></li>
       </ul>
+      <div className={layouts.actions}>
+        <div className={layouts.actionSort}>
+          <span className={layouts.actionSortLabel}>Sort</span> <div className={layouts.actionSortDirection}>Recently</div>
+        </div>
+      </div>
     </div>
-    <div className={layouts.galleries}>
-      {  (new Array(10)).fill(undefined, 0, 10).map((_, key) => {
-        return <React.Fragment key={key}>
-          <div className={layouts.galleryItem} style={{ backgroundImage: "url(/images/dummy/cat.jpg)" }}>
-            <div className={layouts.galleryPostDatetime}>2022-02-01</div>
-            <div className={layouts.galleryTitle}>タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル</div>
-          </div>
-          <div className={layouts.galleryItem} style={{ backgroundImage: "url(/images/dummy/memory.png)" }}>
-            <div className={layouts.galleryPostDatetime}>2022-02-01</div>
-            <div className={layouts.galleryTitle}>タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル</div>
-          </div>
-        </React.Fragment>
-      }) }
+    <div className={layouts.galleriesContainer}>
+      <div className={layouts.galleryDateOfMonth}>September, 2022</div>
+      <div className={layouts.galleries}>
+        {  (new Array(100)).fill(undefined, 0, 100).map((_, key) => {
+          return <React.Fragment key={key}>
+            <div className={layouts.galleryItem} style={{ backgroundImage: "url(/images/dummy/cat.jpg)" }}>
+              <div className={layouts.galleryPostAuthor}>めもりー</div>
+              <div className={layouts.galleryPostDatetime}>2022-02-01</div>
+              <div className={layouts.galleryTitle}>タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル</div>
+            </div>
+            <div className={layouts.galleryItem} style={{ backgroundImage: "url(/images/dummy/memory.png)" }}>
+              <div className={layouts.galleryPostAuthor}>めもりー</div>
+              <div className={layouts.galleryPostDatetime}>2022-02-01</div>
+              <div className={layouts.galleryTitle}>タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル</div>
+            </div>
+          </React.Fragment>
+        }) }
+      </div>
     </div>
     <div className={`${layouts.blogContainer} hidden`}>
       <div className={layouts.blogBody}>

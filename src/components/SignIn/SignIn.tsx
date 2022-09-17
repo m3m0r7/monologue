1b8abfeee3538de1ee9fa097dc39e23970d92c35
m3m0r7 <memory.style.serv@gmail.com>
2022-09-17 13:05:15 +0900

    Update

--- src/components/SignIn/SignIn.tsx
+++ src/components/SignIn/SignIn.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { useURLParameter } from "@/hooks/useURLParameter";
 
 type Props = {};
 
 const SignIn: React.FC<Props> = () => {
+  const { isMonologue, isSignIn } = useURLParameter();
   return <></>
 }
 

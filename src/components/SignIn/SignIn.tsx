030d0698d5b1c132012a09ef99761c5101e954d4
m3m0r7 <memory.style.serv@gmail.com>
2022-09-17 20:40:31 +0900

    Update

--- src/components/SignIn/SignIn.tsx
+++ src/components/SignIn/SignIn.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import { useURLParameter } from "@/hooks/useURLParameter";
+import * as signInStyle from "./signin.module.scss";
 
 type Props = {};
 
 const SignIn: React.FC<Props> = () => {
   const { isMonologue, isSignIn } = useURLParameter();
-  return <></>
+  return <div className={signInStyle.signInOverlay}>
+    <div className={signInStyle.signInContainer}>
+      <div className={signInStyle.signInClose}>
+        <i className={`fa-solid fa-close`}></i>
+      </div>
+      <h2>Sign In</h2>
+      <button className={signInStyle.googleSSOContainer}>
+        <div className={signInStyle.googleSSO}><span><i className="fa-brands fa-google"></i></span></div>
+      </button>
+    </div>
+  </div>
 }
 
 export default SignIn;

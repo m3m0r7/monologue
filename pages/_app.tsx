41cfd3e61f3f3291bab5c587eb4239ff4dac3d18
m3m0r7 <memory.style.serv@gmail.com>
2022-09-19 00:30:05 +0900

    Update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { NextPage } from "next";
 import { AppProps } from "next/app";
 import Head from "next/head";
 import { SessionProvider } from "next-auth/react"
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 
 import "@/css/mixin.scss"
 import "@/css/styles.scss"
@@ -9,20 +10,26 @@ import "@fortawesome/fontawesome-free/css/all.css"
 import { Session } from "next-auth";
 
 const App: NextPage<AppProps<{ session: Session }>> = ({ Component,   pageProps: { session, ...pageProps } }) => {
-  return <SessionProvider session={session}>
-    <Head>
-      <meta name="viewport” content=”width=device-width,initial-scale=1" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@400;700;800;900&display=swap"
-        rel="stylesheet"
-        integrity="sha384-u6hIgRZ/reciX12F2XlDw1e57WFqVuCqnMce6bwBYA/bYDqHEqQOaKpI5yOVbkxJ"
-        crossOrigin="anonymous"
-      />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <title>{process.env.NEXT_PUBLIC_WEB_TITLE}</title>
-    </Head>
-    <Component {...pageProps} />
-  </SessionProvider>
+  const apolloClient = new ApolloClient({
+    uri: '/api/graphql',
+    cache: new InMemoryCache(),
+  });
+  return <ApolloProvider client={apolloClient}>
+    <SessionProvider session={session}>
+      <Head>
+        <meta name="viewport” content=”width=device-width,initial-scale=1" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@400;700;800;900&display=swap"
+          rel="stylesheet"
+          integrity="sha384-u6hIgRZ/reciX12F2XlDw1e57WFqVuCqnMce6bwBYA/bYDqHEqQOaKpI5yOVbkxJ"
+          crossOrigin="anonymous"
+        />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <title>{process.env.NEXT_PUBLIC_WEB_TITLE}</title>
+      </Head>
+      <Component {...pageProps} />
+    </SessionProvider>
+  </ApolloProvider>
 }
 
 export default App
--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,7 +1,14 @@
+// @see: https://zenn.dev/youichiro/articles/9e028d0a3b45e3
 export const resolvers = {
   Query: {
-    entry: () => {
+    entries: () => {
       return [
+        {
+          id: 1,
+        },
+        {
+          id: 1,
+        },
         {
           id: 1,
         },

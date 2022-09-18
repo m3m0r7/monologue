7ff478b28d9d2b92d2d091472b1c08d568801f72
m3m0r7 <memory.style.serv@gmail.com>
2022-09-18 19:25:37 +0900

    Update

--- src/components/Entry/EntryContents.tsx
+++ src/components/Entry/EntryContents.tsx
@@ -4,13 +4,33 @@ import remarkGfm from "remark-gfm";
 import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
 import { xonokai } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import * as entryStyle from "./entry.module.scss";
+import { visit } from "unist-util-visit";
+import { Handlers } from "mdast-util-to-hast";
 
 type Props = {};
+
+const remarkSecretSymbolPlugin = () => {
+  return (tree: any) => visit(tree, "paragraph", (node: any) => {
+    node.children.map((childNode: any) => {
+      if (childNode.type !== 'text') {
+        return;
+      }
+      const matched = childNode.value.match(/(.*?)\[\[([^\]]+?)\]\](.*)/);
+      if (!matched) {
+        return;
+      }
+
+      // secret value.
+      childNode.value = matched[1] + '█'.repeat(matched[2].length) + matched[3];
+    })
+    return;
+  });
+}
 
 const EntryContents: React.FC<PropsWithChildren<Props> & { children: string }> = ({ children }) => {
   return <div className={entryStyle.entryMarkdown}>
     <ReactMarkdown
-      remarkPlugins={[remarkGfm]}
+      remarkPlugins={[remarkSecretSymbolPlugin, remarkGfm]}
       components={{
         code({node, inline, className, children, ...props}) {
           const match = /language-(\w+)/.exec(className || '')

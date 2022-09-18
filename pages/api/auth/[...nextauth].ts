15b0ec4f38e0a5e4affd95c727bfc24d64782369
m3m0r7 <memory.style.serv@gmail.com>
2022-09-18 18:21:55 +0900

    Update

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,11 @@
-import NextAuth from 'next-auth'
+import NextAuth, { NextAuthOptions } from 'next-auth'
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions ={
+export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
     }),
   ],
   session: {
@@ -13,14 +13,12 @@ export const authOptions ={
     maxAge: 30 * 24 * 60 * 60,
   },
   callbacks: {
-    //jwtが作成・更新された時に呼ばれる
     async jwt({ token, account }) {
       if (account) {
         token.accessToken = account.access_token;
       }
       return token;
     },
-    //セッションがチェックされた時に呼ばれる
     async session({ session, token, user }) {
       session.accessToken = token.accessToken;
       return session;

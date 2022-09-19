e18b5a89bc7789f8f9633a77944a063d95f927db
m3m0r7 <memory.style.serv@gmail.com>
2022-09-19 21:04:25 +0900

    Update

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,23 +1,12 @@
 // @see: https://zenn.dev/youichiro/articles/9e028d0a3b45e3
 import { addEntry } from "@/graphql/resolvers/mutation/addEntry";
+import { getEntries } from "@/graphql/resolvers/query/getEntries";
 
 export const resolvers = {
   Mutation: {
     addEntry: addEntry
   },
   Query: {
-    entries: () => {
-      return [
-        {
-          id: 1,
-        },
-        {
-          id: 1,
-        },
-        {
-          id: 1,
-        },
-      ]
-    }
+    getEntries: getEntries
   }
 }
---
+++ src/graphql/resolvers/query/getEntries.ts
@@ -0,0 +1,11 @@
+import { prisma } from '@/graphql/context';
+import { QueryResolvers } from "@/@types/resolvers-types";
+
+export const getEntries: QueryResolvers['getEntries'] = async (
+  parent,
+  args,
+  context,
+  info
+) => {
+  return await prisma.entry.findMany();
+};

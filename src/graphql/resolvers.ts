cef9f3eee71d4400b59c8e0ef3a85d297240f8c2
m3m0r7 <memory.style.serv@gmail.com>
2022-09-20 01:06:15 +0900

    Update

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,6 +1,9 @@
 // @see: https://zenn.dev/youichiro/articles/9e028d0a3b45e3
 import { addEntry } from "@/graphql/resolvers/mutation/addEntry";
 import { getEntries } from "@/graphql/resolvers/query/getEntries";
+import { GraphQLScalarType } from "graphql/type";
+import { Kind } from "graphql/language";
+import dayjs, { Dayjs } from "dayjs";
 
 export const resolvers = {
   Mutation: {
@@ -8,5 +11,12 @@ export const resolvers = {
   },
   Query: {
     getEntries,
-  }
+  },
+  DateTime: new GraphQLScalarType({
+    name: 'DateTime',
+    description: 'Date custom scalar type',
+    parseValue(value: any) {
+      return dayjs(value);
+    },
+  })
 }
